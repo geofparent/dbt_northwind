7aa7e5360b283d107faf80c5cff454f962a704ed
geofparent <62191727+geofparent@users.noreply.github.com>
2022-12-01 21:02:19 -0800

    first commit

---
+++ dbt_northwind/models/stage/stg_customer.sql
@@ -0,0 +1,26 @@
+WITH source AS (
+
+  SELECT * FROM {{ source('northwind','customer')}}
+)
+
+SELECT
+  id,
+  company,
+  last_name,
+  first_name,
+  email_address,
+  job_title,
+  business_phone,
+  home_phone,
+  mobile_phone,
+  fax_number,
+  address,
+  city,
+  state_province,
+  zip_postal_code,
+  country_region,
+  web_page,
+  notes,
+  attachments,
+  current_timestamp() AS insertion_timestamp
+FROM source
